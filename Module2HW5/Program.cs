37805acf5ef823ec10d6d418b7f2ec2b6619d749
LadogaX <ladogax@gmail.com>
2021-11-25 01:35:36 +0200

    Empty project

---
+++ Module2HW5/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Modul2HW1;
+using Modul2HW5;
+using Module2HW5.Services;
+using Module2HW5.Services.Abstractions;
+
+namespace Module2HW5
+{
+   public class Program
+    {
+       public static void Main()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<ILogger, Logger>()
+                .AddTransient<IFileService, FileService>()
+                .AddTransient<IConfigService, ConfigService>()
+                .AddTransient<Starter>()
+                .BuildServiceProvider();
+
+            var starter = serviceProvider.GetService<Starter>();
+            try
+            {
+                starter.Run();
+            }
+            finally
+            {
+                starter.Logger.Close();
+            }
+        }
+    }
+}
